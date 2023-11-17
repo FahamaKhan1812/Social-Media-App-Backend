b8d2e8558a73de3ae11baf641bfe4cc2d693c8e6
fahama khan <fahamakhan990@gmail.com>
2023-11-17 12:36:15 +0500

    add new error codes

--- SocialMedia.Application/Enums/ErrorCode.cs
+++ SocialMedia.Application/Enums/ErrorCode.cs
@@ -8,5 +8,9 @@ public enum ErrorCode
     IdentityCreationFailed = 202,
     IdentityUserNotFound = 203,
     IncorrectPassword = 204,
+
+    //Application Error should be in the range of 300 - 399
+    PostUpdateUserNotPossible = 300,
+    PostDeleteNotPossible = 301,
     UnknownError = 999
 }
